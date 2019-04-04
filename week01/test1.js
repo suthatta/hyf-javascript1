a0b150499e3eeec60f655788d6480db4a9bfc1b0
Suthatta Lindstrii <psuthatta@gmail.com>
2019-04-04 07:57:56 +0200

    Change structure

--- week01/test1.js
+++ week01/test1.js
@@ -4,18 +4,14 @@
 /*Halo, dunia! // Indonesian
 Ciao, mondo! // Italian
 Hola, mundo! // Spanish*/
-console.log('1.Hello World ---------------');
 console.log("Hello World" +' // English');
-console.log("Ciao, mondo!" +' // Italian');
-console.log("Hola, mundo" +' // Spanska');
+console.log("halo dunia" +' // Indoniska');
 console.log("Sawad dee" +' // Thailandska');
 
 
 //2. console.log('I'm awesome');
-console.log('2.-------------------');
 console.log("I'm awesome");
 // 3
-console.log('3.------------------');
 let x;
 console.log("the value of x will be: whateverYouThinkItWillLog");
 console.log("the value of my variable x will be: ", (x));
@@ -25,7 +21,6 @@ console.log("the value of my variable x will be: ", (x));
 console.log("Type of X : ", (typeof(x)));
 
 // 4. Y 
-console.log('4.-------------------');
 let y;
 console.log("the value of my string will be: whateverYouThinkItWillLog ");
 console.log(typeof(y));
@@ -34,7 +29,7 @@ console.log('The value of Y is = ', (y));
 console.log("the value of my string will be: whateverYouThinkItWillLog");
 console.log(typeof(y));
 // 5 round number
-console.log('5.-------------------');
+
 let z= 7.25;
 console.log('Print z = ', z);
 aa = z;
@@ -42,7 +37,6 @@ console.log('Print aa = ', Math.round(aa));
 x = Math.max(aa, z);
 console.log('Print highest value = ', x);
 //6 Array
-console.log('6.-------------------');
 let myArray=[];
 console.log("the value of my Array will be: whateverYouThinkItWillLog");
 console.log(typeof(myArray));
@@ -53,7 +47,7 @@ myArray[3]= "Daan's favorite animal ('baby pig')";
 console.log('Print myFavoritAnimals = ', myArray);
 
 //7 more string
-console.log('7.-------------------');
+
 let myString = "this is a test";
 console.log(myString);
 //Find String length
@@ -61,22 +55,21 @@ stringLength = myString.length;
 console.log('Pring Strings length = ', stringLength);
 
 //8 
-console.log('8.-------------------');
-t = 'Text String';
-n = 123456;
+text = 'Text String';
+number = 123456;
 array = ['array 1', 'array 2'];
 boolean = true;
-console.log("The value of my variable t is: " + t);
-console.log("The value of my variable n is: " + n);
+console.log("The value of my variable text is: " + text);
+console.log("The value of my variable number is: " + number);
 console.log("The value of my variable array is: " + array);
-console.log("The value of my variable b is: " + boolean);
+console.log("The value of my variable boolean is: " + boolean);
 console.log("the value of my Array will be: whateverYouThinkItWillLog");
-console.log("Type of variable t is: " + typeof(t));
-console.log("Type of variable n is: " + typeof(n));
+console.log("Type of variable text is: " + typeof(text));
+console.log("Type of variable number is: " + typeof(number));
 console.log("Type of variable array is: " + typeof(array));
 console.log("Type of variable boolean is: " + typeof(boolean));
 // compare type of 
-if (typeof(t)== typeof(n)==typeof(array)==typeof(boolean)){
+if (typeof(text)== typeof(number)==typeof(array)==typeof(boolean)){
     console.log('SAME TYPE');
 }
 else {
@@ -92,7 +85,7 @@ console.log(' 7 % 3 =[7/3= 2.1] then the Value of 7%3 = 1');
 console.log('Print 7 % 3 = ', x);
 //10
 console.log('10.-------------------');
-let person ={firstname: "Anna", lastName:"Svensson", age:45};
+let person ={firstname: "Anna", lastName:"Svensson", age:45, workstatus:true};
 console.log(person);
 //Infinity
 let maxNumber = Math.pow(10, 1000); // max positive number 10*10(1000times)
